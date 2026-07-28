152145dff2b679de5e6ec3e0431b72bec5504fb5
fxsharafuddin904-pixel <fxsharafuddin904@gmail.com>
2026-07-29 00:57:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Music website loaded.");
+function nextStep() {
+  const phone = document.getElementById("phone").value.trim();
+
+  if (!phone) {
+    alert("Please enter your number.");
+    return;
+  }
+
+  document.getElementById("step1").classList.add("hidden");
+  document.getElementById("step2").classList.remove("hidden");
+}
+
+
+function submitMessage() {
+  const message = document.getElementById("message").value.trim();
+
+  if (!message) {
+    alert("Please write your custom message.");
+    return;
+  }
+
+  document.getElementById("step2").classList.add("hidden");
+  document.getElementById("step3").classList.remove("hidden");
+
+  const music = document.getElementById("music");
+
+  music.currentTime = 0;
+
+  music.play().catch(() => {
+    alert("Tap the page once to allow music playback.");
+  });
+    }
